36ff70b82c6f24e56b2d6dec84cd3d76ddf32159
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-07-29 13:32:11 -0500

    Added more styles to the dashboard

--- src/js/library.js
+++ src/js/library.js
@@ -1,4 +1,8 @@
 function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
   this.info = function () {
     return console.log(
       `${title} by ${author}, ${pages}, ${isRead ? "read" : "not read yet"} `
